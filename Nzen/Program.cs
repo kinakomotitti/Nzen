fec04e4d90f0d0238c37906f9d8e1943e0116eec
きなこもちっち <kinakomotitti0210@yahoo.co.jp>
2019-05-12 22:34:42 +0900

    　

--- Nzen/Program.cs
+++ Nzen/Program.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
-namespace Nzen
+﻿namespace Nzen
 {
+    #region using
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    #endregion
+
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) =>
             CreateWebHostBuilder(args).Build().Run();
-        }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                   .UseStartup<Startup>();
     }
 }
