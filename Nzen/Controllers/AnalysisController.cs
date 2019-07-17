ec72fd7a351018dc4657f9d4c427126a3475201f
きなこもちっち <kinakomotitti0210@yahoo.co.jp>
2019-07-17 21:46:16 +0900

    add analysis page

---
+++ Nzen/Controllers/AnalysisController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Nzen.Controllers
+{
+    #region using
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CotohaAPI;
+    using Microsoft.AspNetCore.Diagnostics;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Nzen.Manager;
+    using Nzen.Models;
+    #endregion
+
+
+    public class AnalysisController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ResultData(TargetEventInfo info)
+        {
+            AnalysisModel model = new AnalysisModel();
+
+            var tokenResult = HttpClientManager.GetAccessToken().Result;
+
+            if (tokenResult.StatusCode != HttpStatusCode.Created) return View("Index");
+
+            model.KeywordResult= await HttpClientManager.ExtractionKeywords(new CotohaAPI.Models.KeyWordRequest()
+            {
+
+            });
+
+            model.SentimentResult= await HttpClientManager.SentimentAnalysis(new CotohaAPI.Models.SentimentRequest()
+            {
+
+            });
+
+            return View("Result");
+        }
+    }
+}
---
+++ Nzen/Models/Analysis.cs
@@ -0,0 +1,38 @@
+﻿namespace Nzen.Models
+{
+    #region using
+    using CotohaAPI.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    #endregion
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AnalysisModel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public KeyWordResponce KeywordResult { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SentimentResponce SentimentResult { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TargetEventInfo info { get; set; }
+    }
+
+    public class TargetEventInfo
+    {
+        public string GroupId { get; set; }
+
+        public DateTime EventDate { get; set; }
+    }
+}
