4b3d32f5e685fba54c937ef270eb0f1483f8d58b
きなこもち <sugane0210@gmail.com>
2019-07-22 20:38:46 +0900

    元に戻す

--- Nzen/Program.cs
+++ Nzen/Program.cs
@@ -12,13 +12,6 @@
             CreateWebHostBuilder(args)
             .CaptureStartupErrors(true)
             .UseSetting(WebHostDefaults.DetailedErrorsKey, "true")
-            .UseKestrel(options=>
-            {
-                options.Listen(IPAddress.Loopback, 5000, listenOptions =>
-                  {
-                      listenOptions.UseHttps("Nzen.pfx", "839da23a-df3b-4a61-a280-6118f7c52d7b");
-                  });
-            })
             .Build()
             .Run();
 
